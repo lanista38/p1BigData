ac5ce5c03ddb44e45df78cf5d837705e75028b51
jorgec <jorgec2013@gmail.com>
2018-10-18 22:17:49 -0400

    + distinct usernames ex

---
+++ ex3/src/main/java/ex3/TDistincScreenNameMain.java
@@ -0,0 +1,36 @@
+package ex3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class TDistincScreenNameMain  {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Wrong Args");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(ex3.TDistincScreenNameMain.class);
+        job.setJobName("Count TweetsbyUsr");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(ex3.TDistincScreenNameMapper.class);
+        job.setReducerClass(ex3.TDistincScreenNameReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ ex3/src/main/java/ex3/TDistincScreenNameMapper.java
@@ -0,0 +1,34 @@
+package ex3;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+import twitter4j.User;
+
+import java.io.IOException;
+
+public class TDistincScreenNameMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String rawTweet = value.toString();
+
+        try {
+            Status status = TwitterObjectFactory.createStatus(rawTweet);
+            User user = status.getUser();
+            String screenName = user.getScreenName();
+            System.out.println(screenName);
+
+            context.write(new Text(screenName), new IntWritable(1));
+        }
+
+        catch(TwitterException e) {
+
+        }
+    }
+}
